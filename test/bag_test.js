e07fa953fd8d56ced8a76b3ae4e8cb3a92802c71
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-06-25 07:19:14 +0200

    correct indent of bag_test.js

--- test/bag_test.js
+++ test/bag_test.js
@@ -11,60 +11,60 @@ Counter.prototype.increment = function(){
 
 describe('Bag', function(){
     it('should exist', function(){
-	expect(Bag).to.exist;
+        expect(Bag).to.exist;
     });
 
     it('should be a constructor', function(){
-	expect(Bag).to.be.a('function');
+        expect(Bag).to.be.a('function');
     });
 
     describe('object', function(){
-	var bag;
+        var bag;
 
-	beforeEach(function(){
-	    bag = new Bag();
-	});
+        beforeEach(function(){
+            bag = new Bag();
+        });
 
-	['add', 'remove', 'forEach'].forEach(function(methodName){
-	    it('should respond to ' + methodName, function(){
-		expect(bag).to.respondTo(methodName);
-	    });
-	});
+        ['add', 'remove', 'forEach'].forEach(function(methodName){
+            it('should respond to ' + methodName, function(){
+                expect(bag).to.respondTo(methodName);
+            });
+        });
     });
 
     describe('interactions', function(){
-	var bag;
+        var bag;
 
-	beforeEach(function(){
-	    bag = new Bag();
-	});
+        beforeEach(function(){
+            bag = new Bag();
+        });
 
-	it('should start out with zero elements', function(){
-	    var counter = new Counter;
+        it('should start out with zero elements', function(){
+            var counter = new Counter;
 
-	    bag.forEach(counter.increment.bind(this));
+            bag.forEach(counter.increment.bind(this));
 
-	    expect(counter.count).to.equal(0);
-	});
+            expect(counter.count).to.equal(0);
+        });
 
-	it('should add an element', function(){
-	    var counter = new Counter;
-	    bag.add({});
+        it('should add an element', function(){
+            var counter = new Counter;
+            bag.add({});
 
-	    bag.forEach(counter.increment.bind(counter));
+            bag.forEach(counter.increment.bind(counter));
 
-	    expect(counter.count).to.equal(1);
-	});
+            expect(counter.count).to.equal(1);
+        });
 
-	it('should remove an element by id', function(){
-	    var counter = new Counter;
-	    var id = bag.add({});
-	    bag.remove(id);
+        it('should remove an element by id', function(){
+            var counter = new Counter;
+            var id = bag.add({});
+            bag.remove(id);
 
-	    bag.forEach(counter.increment.bind(counter));
+            bag.forEach(counter.increment.bind(counter));
 
-	    expect(counter.count).to.equal(0);
-	});
+            expect(counter.count).to.equal(0);
+        });
     });
 
 });
